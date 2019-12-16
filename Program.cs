74ac9dd318b20b6555a96911abe21b8de198ba91
Evgenij-Kovpak <58892723+Evgenij-Kovpak@users.noreply.github.com>
2019-12-16 21:29:24 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,63 +4,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace HomeTask_10
+namespace HomeTask_11
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int N;
-            Int32.TryParse(Console.ReadLine(), out N);
-            int[,] array = new int[N, N];
-            Random random = new Random();
-            int rand;
-            int sum = 0;
-
+           
+            for (int i = 0; i < 3; i++)
             {
-                {
-                    for (int i = 0; i < N; i++)
-                    {
-                        for (int j = 0; j < N; j++)
-                        {
-                            rand = random.Next(1, 3);
-                            array[i, j] = rand;
-                            Console.Write(array[i, j]);
-
-                        }
-                        Console.WriteLine();
-                    }
-                    {
-                        {
-                            for (int i = 0; i < N; i++)
-                                sum += array[i, i];
-
-                        }
-                    }
-                    {
-                        {
-                            for (int i = 0; i < N; i++)
-                            {
-                                for (int j = 0; j < N; j++)
-                                {
-
-                                    if (j == N - i - 1)
-                                    {
-                                        sum += array[i, j];
-                                    }
-                                    if (N % 2 != 0 && i == N / 2 + 1 && j == N / 2 + 1)
-                                    {
-                                        sum -= array[N / 2 + 1, N / 2 + 1];
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-
-                Console.Write(sum);
+                Console.Write("Введите n= ");
+                int a = Convert.ToInt32(Console.ReadLine());
+              
+                Console.WriteLine(fakt(a));
                 Console.ReadKey();
-
             }
-        }    }
+        }
+        
+        
+        static Int64 fakt(int n)
+        {
+            Int64 f;
+            if (n < 1)
+                f = 1;
+            else
+                f = fakt(n - 1) * n;
+            return f;
+        }
+    }
 }
