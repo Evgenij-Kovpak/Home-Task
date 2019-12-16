311c3d9dacca02b5605d332e62f0bf1ad235b8cf
Evgenij-Kovpak <58892723+Evgenij-Kovpak@users.noreply.github.com>
2019-12-16 21:27:11 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeTask_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N;
+            Int32.TryParse(Console.ReadLine(), out N);
+            int[,] array = new int[N, N];
+            Random random = new Random();
+            int rand;
+            int sum = 0;
+
+            {
+                {
+                    for (int i = 0; i < N; i++)
+                    {
+                        for (int j = 0; j < N; j++)
+                        {
+                            rand = random.Next(1, 3);
+                            array[i, j] = rand;
+                            Console.Write(array[i, j]);
+
+                        }
+                        Console.WriteLine();
+                    }
+                    {
+                        {
+                            for (int i = 0; i < N; i++)
+                                sum += array[i, i];
+
+                        }
+                    }
+                    {
+                        {
+                            for (int i = 0; i < N; i++)
+                            {
+                                for (int j = 0; j < N; j++)
+                                {
+
+                                    if (j == N - i - 1)
+                                    {
+                                        sum += array[i, j];
+                                    }
+                                    if (N % 2 != 0 && i == N / 2 + 1 && j == N / 2 + 1)
+                                    {
+                                        sum -= array[N / 2 + 1, N / 2 + 1];
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+
+                Console.Write(sum);
+                Console.ReadKey();
+
+            }
+        }    }
+}
