995439eca4759f308909f2399e5ce43cec2b2a8f
Evgenij-Kovpak <58892723+Evgenij-Kovpak@users.noreply.github.com>
2019-12-19 23:30:23 +0200

    Add files via upload
    My code

--- Program.cs
+++ Program.cs
@@ -4,45 +4,157 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace HomeTask_12
+namespace Citys
 {
     class Program
-   
     {
-        static void Swap(ref int e1, ref int e2)
-        {
-            var temp = e1;
-            e1 = e2;
-            e2 = temp;
-        }
-
-        static int [] BubbleSort(int [] array)
-        {
-            var len = array.Length;
-            for (var i = 1; i < len; i++)
-            {
-                for (var j = 1; j < len - i; j++)
-                {
-                    if(array[j] > array[j + 1])
-                    {
-                        Swap(ref array[j], ref array[j + 1]);
-                    }
-                }
-            }
-            return array;
-        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Bubble sort");
-            Console.Write("Input array element ");
-            var parts = Console.ReadLine().Split(new[] { " ", ",", ";" }, StringSplitOptions.RemoveEmptyEntries);
-            var array = new int[parts.Length];
-            for(int i =0; i < parts.Length; i++)
+            string str = "Kharkv=1431000,350;Kiev=2804000,839;Las Vegas=603400,352";
+
+            string[] split = str.Split(';', ',', '=');
+
+            int count = 1;
+
+            for (int i = 0; i < str.Length; i++)
             {
-                array[i] = Convert.ToInt32(parts[i]);
+
+                if (str[i] == ';')
+                {
+                    count++;
+                }
+
             }
-            Console.WriteLine("Отсортированный массив: {0}", string.Join(", ", BubbleSort(array)));
-            Console.ReadLine();
+
+            string[] split6 = new string[count];
+            int p = 0;
+            for (int j = 0; j < count; j++)
+
+            {
+                split6[j] = split[p];
+                p = p + 3;
+            }
+
+            string[] split2 = new string[count];
+            int k = 0;
+            for (int j = 0; j < count; j++)
+
+            {
+                split2[j] = split[k];
+                k = k + 3;
+            }
+
+            string tmp;
+            for (int i = 0; i < count - 1; i++)
+
+            {
+                for (int j = 0; j < count - i - 1; j++)
+                {
+                    if (split2[j + 1].Length > split2[j].Length)
+
+                    {
+                        tmp = split2[j + 1];
+                        split2[j + 1] = split2[j];
+                        split2[j] = tmp;
+                    }
+
+                }
+
+
+            }
+          
+            int[] split3 = new int[count];
+            int l = 1;
+            for (int j = 0; j < count; j++)
+
+            {
+                split3[j] = Convert.ToInt32(split[l]);
+                l = l + 3;
+            }
+
+            int tmp1;
+            for (int i = 0; i < count - 1; i++)
+
+            {
+                for (int j = 0; j < count - i - 1; j++)
+                {
+                    if (split3[j + 1] > split3[j])
+
+                    {
+                        tmp1 = split3[j + 1];
+                        split3[j + 1] = split3[j];
+                        split3[j] = tmp1;
+                    }
+
+                }
+
+            }
+
+            string outcity = Convert.ToString(split3[0]);
+            for (int i = 0; i < split.Length; i++)
+            {
+                if (string.Equals(outcity, split[i]))
+                {
+                    Console.WriteLine("Most populated:" + split[i - 1] + '(' + outcity + " people)");
+                    break;
+                }
+            }
+
+            Console.WriteLine("Longest name:" + split2[0] + '(' + split2[0].Length + " letters )");
+
+            int[] split5 = new int[count];
+            int m = 1;
+            for (int j = 0; j < count; j++)
+
+            {
+                split5[j] = Convert.ToInt32(split[m]);
+                m = m + 3;
+            }
+
+            int[] split4 = new int[count];
+            int n = 2;
+            for (int j = 0; j < count; j++)
+
+            {
+                split4[j] = Convert.ToInt32(split[n]);
+                n = n + 3;
+            }
+
+            double[] dencity = new double[count];
+            for (int denc = 0; denc < count; denc++)
+
+            {
+                dencity[denc] = Convert.ToDouble(split5[denc]) / Convert.ToDouble(split4[denc]);
+            }
+            Console.WriteLine("Dencity :");
+
+
+            for (int i = 0; i < count; i++)
+
+            {
+                Console.WriteLine(split6[i] + '-' + string.Format("{0:N2}", dencity[i]));
+                Console.WriteLine();
+            }
+
+            int tmp2;
+            for (int i = 0; i < count - 1; i++)
+
+            {
+                for (int j = 0; j < count - i - 1; j++)
+                {
+                    if (split4[j + 1] > split4[j])
+
+                    {
+                        tmp2 = split4[j + 1];
+                        split4[j + 1] = split4[j];
+                        split4[j] = tmp2;
+                    }
+
+                }
+
+            }
+            
+            Console.ReadKey();
         }
     }
 }
